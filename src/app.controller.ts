f13ead6c499e385c9f843c73aea29c9325194a38
jinhoe <jinhoe.kim@imweb.me>
2024-12-09 10:37:30 +0900

    feat: 밸리데이션 파이프 세팅, 최초 사이트정보 받는 API에 validation 적용

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { GetSiteInfoRequestDto } from './dto/get-site-info-request.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getSiteInfo(@Query() query: { siteCode: string }): Promise<string> {
+  async getSiteInfo(@Query() query: GetSiteInfoRequestDto): Promise<string> {
     return this.appService.getSiteInfo(query);
   }
 }
--- src/dto/get-site-info-request.dto.ts
+++ src/dto/get-site-info-request.dto.ts
@@ -1,3 +1,10 @@
+import { Exclude, Expose } from 'class-transformer';
+import { IsString, Length } from 'class-validator';
+
+@Exclude()
 export class GetSiteInfoRequestDto {
+  @IsString()
+  @Length(22, 22)
+  @Expose()
   siteCode: string;
 }
