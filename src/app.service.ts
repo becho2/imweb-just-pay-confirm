7744f805e3cab0b3eeee37784e8d6b6a4e6bbdd3
jinhoe <jinhoe.kim@imweb.me>
2024-12-09 10:08:08 +0900

    feat: prisma 세팅, 최초 siteCode 받아서 저장하는 API 작성

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { GetSiteInfoRequestDto } from './dto/get-site-info-request.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  prisma = new PrismaClient();
+  constructor() {}
+
+  async getSiteInfo(query: GetSiteInfoRequestDto): Promise<string> {
+    const site = await this.prisma.site.findUnique({
+      where: {
+        site_code: query.siteCode,
+        is_deleted: 'N',
+      },
+    });
+
+    if (!site) {
+      await this.prisma.site.create({
+        data: {
+          site_code: query.siteCode,
+        },
+      });
+    }
+
     return 'Hello World!';
   }
 }
---
+++ src/dto/get-site-info-request.dto.ts
@@ -0,0 +1,3 @@
+export class GetSiteInfoRequestDto {
+  siteCode: string;
+}
